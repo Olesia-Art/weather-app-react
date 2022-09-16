fa0093efba20bdfeec8e6e6b5c4b3fae7c37d9fb
Olesia-Art <olesiasmail1@gmail.com>
2022-09-16 16:58:19 +0300

    Changed the structure of the application

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import React from "react";
 
 import "./App.css";
-import Cities from "./Cities";
-import Details from "./Details";
+
 import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="container">
-      <div className="Weather">
-        <div className="card">
-          <div className="card-body">
-            <Cities />
-            <Weather />
-            <h1 id="cityName">Kyiv</h1>
-            <h2>
-              <span id="date">Monday</span>
-            </h2>
-            <h3 id="description">Clear</h3>
-            <Details />
-            <div className="weather-forecast" id="forecast"></div>
-          </div>
-        </div>
+    <div className="App">
+      <div className="container">
+        <Weather />
+
+        <footer>
+          <a href="https://github.com/Olesia-Art/weather-app">
+            Open-source code{" "}
+          </a>
+          by Olesia Artibiakina
+        </footer>
       </div>
-      <a href="https://github.com/Olesia-Art/weather-app">Open-source code </a>
-      by Olesia Artibiakina
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,36 +1,75 @@
 import React from "react";
-import "./App.css";
+import Cities from "./Cities";
+import "./Weather.css";
 
-export default function App() {
+export default function Weather() {
   return (
-    <form className="search-form" id="searchCity">
+    <div className="Weather">
+      <Cities />
+      <br />
+      <form className="search-form" id="searchCity">
+        <div className="row">
+          <span className="col-6">
+            <input
+              type="search"
+              placeholder="Enter a city"
+              id="search-text-input"
+              autocomplete="off"
+              autoFocus="on"
+              className="form-control shadow-sm"
+            />
+          </span>
+          <span className="col-2">
+            <input
+              type="submit"
+              value="Search"
+              className="search btn btn-primary "
+            />
+          </span>
+          <span className="col-2">
+            <input
+              type="submit"
+              value="Current position"
+              className="btn btn-success"
+              id="currentButton"
+            />
+          </span>
+        </div>
+      </form>
+      <h1 id="city">Kyiv</h1>
+      <ul>
+        <li>Monday 12:30</li>
+        <li>Cloudy</li>
+      </ul>
       <div className="row">
-        <span className="col-6">
-          <input
-            type="text"
-            placeholder="Enter a city"
-            id="search-text-input"
-            autocomplete="off"
-            autofocus="on"
-            className="form-control shadow-sm"
+        <div className="col-6">
+          <img
+            src="http://openweathermap.org/img/wn/10d@2x.png"
+            alt=""
+            id="image"
+            className="float-left"
           />
-        </span>
-        <span className="col-2">
-          <input
-            type="submit"
-            value="Search"
-            className="search btn btn-primary"
-          />
-        </span>
-        <span className="col-2">
-          <input
-            type="submit"
-            value="Current position"
-            className="btn btn-success"
-            id="currentButton"
-          />
-        </span>
+          <span className="temperature">19</span>
+          <span className="temperature-units">
+            <button id="celsius" className="active">
+              °C
+            </button>{" "}
+            |<button id="fahrenheit">°F</button>
+          </span>
+        </div>
+        <div className="col-6">
+          <ul className="applications">
+            <li>
+              Humidity: <span id="humidity">66</span>%
+            </li>
+            <li>
+              {" "}
+              Wind: <span id="wind">8</span> km/h
+            </li>
+            <li>Precipitation: 13%</li>
+          </ul>
+        </div>
       </div>
-    </form>
+    </div>
   );
 }
